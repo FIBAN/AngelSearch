6c95df0961f6b98307c4dd6c9377c81d437765df
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2017-08-02 10:02:29 +0300

    improved profile editing

--- frontend/src/app/profile.component.ts
+++ frontend/src/app/profile.component.ts
@@ -10,20 +10,41 @@ import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'angel-profile',
   templateUrl: 'profile.component.html',
-  styleUrls: []
+  styleUrls: ['profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   angel: Angel;
   newEmail: string;
+  activeEdit: string;
+  edits: any;
 
   constructor(
     private angelService: AngelService,
     private authService: AuthService,
     private route: ActivatedRoute) {
+    this.edits = {};
   }
 
   ngOnInit(): void {
-    this.angelService.getMyAngel().then(angel => this.angel = angel);
+    this.angelService.getMyAngel().then(angel => {
+      this.angel = angel;
+      for(let k in angel){
+        if(angel.hasOwnProperty(k)) this.edits[k] = angel[k];
+      }
+    });
+  }
+
+  startEdit(key): void {
+    this.activeEdit = key;
+  }
+
+  stopEdit(): void {
+    this.activeEdit = "";
+  }
+
+  saveEdit(key): void {
+    this.angel[key] = this.edits[key];
+    this.angelService.updateAngel(this.angel).then(() => location.reload())
   }
 
   changeEmail(): void {
