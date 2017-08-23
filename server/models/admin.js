92d478cfeacd30a21fc3edd36008d99145a5ddd7
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2017-08-23 16:57:02 +0300

    fixed token expiry in the backend

--- server/models/admin.js
+++ server/models/admin.js
@@ -3,9 +3,9 @@ const auth0 = require('./auth0-management');
 
 
 exports.getUsers = function () {
-    return auth0.then(client => client.getUsers());
+    return auth0.client().then(client => client.getUsers());
 };
 
 exports.setUserAngelId = function (auth0Id, angelId) {
-    return auth0.then(client => client.updateAppMetadata({id: auth0Id}, {angel_id: angelId}));
+    return auth0.client().then(client => client.updateAppMetadata({id: auth0Id}, {angel_id: angelId}));
 };
--- server/models/auth0-management.js
+++ server/models/auth0-management.js
@@ -2,20 +2,24 @@
 require('dotenv').config();
 
 const AuthenticationClient = require('auth0').AuthenticationClient;
-const auth0 = new AuthenticationClient({
-    domain: 'fiban.eu.auth0.com',
-    clientId: process.env.AUTH0_CLIENTID,
-    clientSecret: process.env.AUTH0_SECRET,
-});
+
 const ManagementClient = require('auth0').ManagementClient;
 
-module.exports = auth0.clientCredentialsGrant({
-    audience: 'https://fiban.eu.auth0.com/api/v2/'
-}).then(response => {
-    return new ManagementClient({
-        token: response.access_token,
-        domain: 'fiban.eu.auth0.com'
+module.exports.client = function () {
+    const auth0 = new AuthenticationClient({
+        domain: 'fiban.eu.auth0.com',
+        clientId: process.env.AUTH0_CLIENTID,
+        clientSecret: process.env.AUTH0_SECRET,
     });
-}).catch(err => {
-    console.error("didn't get auth0 credentials", err);
-});
+
+    return auth0.clientCredentialsGrant({
+        audience: 'https://fiban.eu.auth0.com/api/v2/'
+    }).then(response => {
+        return new ManagementClient({
+            token: response.access_token,
+            domain: 'fiban.eu.auth0.com'
+        });
+    }).catch(err => {
+        console.error("didn't get auth0 credentials", err);
+    });
+};
