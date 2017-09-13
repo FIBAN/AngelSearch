33689122b6522941dba5e24e646a702409dcdc3c
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2017-09-13 13:51:34 +0300

    improved footer and header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Angulartics2GoogleAnalytics } from "angulartics2";
 @Component({
   selector: 'angel-search',
   templateUrl: 'app.component.html',
-  styleUrls : ['app.component.css']
+  styleUrls : ['app.component.scss']
 })
 export class AppComponent{
   authenticated: boolean;
